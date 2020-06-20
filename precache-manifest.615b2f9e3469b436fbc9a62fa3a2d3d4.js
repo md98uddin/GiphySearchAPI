85d62559cef2eeeddef25cd0e70d3f6479d34410
md uddin <mduddin1998@yahoo.com>
2020-06-20 15:50:31 -0400

    Updates

---
+++ precache-manifest.615b2f9e3469b436fbc9a62fa3a2d3d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66bf9e0144255e7e1ecdbb5af8481754",
+    "url": "/GiphySearchAPI/index.html"
+  },
+  {
+    "revision": "f52b1792813f0716826d",
+    "url": "/GiphySearchAPI/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "26e784072e2bf67b3648",
+    "url": "/GiphySearchAPI/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "f52b1792813f0716826d",
+    "url": "/GiphySearchAPI/static/js/2.1928323a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GiphySearchAPI/static/js/2.1928323a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26e784072e2bf67b3648",
+    "url": "/GiphySearchAPI/static/js/main.122bd836.chunk.js"
+  },
+  {
+    "revision": "1f7040ba7721f9c2f546",
+    "url": "/GiphySearchAPI/static/js/runtime-main.1e33a330.js"
+  }
+]);
