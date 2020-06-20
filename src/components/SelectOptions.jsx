048df35fb0c8cdf910a3229c77f189c7e33f9038
rickyricky787 <rrodriguez2017@gmail.com>
2020-06-20 15:47:14 -0400

    Working on filter

--- src/components/SelectOptions.jsx
+++ src/components/SelectOptions.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 
-const SelectOptions = () => {
+const SelectOptions = ({filterSelection, handleChange}) => {
   return (
-    <select>
-      <option value="none">SORT BY</option>
-      <option value="recent">Most Recent</option>
-      <option value="oldest">Oldest</option>
-    </select>
+    <select
+    value={filterSelection}
+    onChange={handleChange}
+    >
+    <option value="relevance">Relevance</option>
+    <option value="recent">Most Recent</option>
+    <option value="oldest">Oldest</option>
+  </select>
   );
 };
 
